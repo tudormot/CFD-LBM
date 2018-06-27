d474c1cfb2bdeab0c8a6f2ed82753c90780cf783
gilbertolem <gilbertolemc@gmail.com>
2018-06-27 15:29:16 +0200

    Added old code and gitignore

---
+++ src/initLB.c
@@ -0,0 +1,15 @@
+#include "initLB.h"
+
+int readParameters(int *xlength, double *tau, double *velocityWall, unsigned int *timesteps, unsigned int *timestepsPerPlotting, int argc, char *argv[]){
+  /* TODO */
+
+
+
+  return 0;
+}
+
+
+void initialiseFields(double *collideField, double *streamField, unsigned int *flagField, int xlength){
+  /* TODO */
+}
+
