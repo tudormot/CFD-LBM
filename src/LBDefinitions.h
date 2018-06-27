caa41c25720c3015aad345b2c01e9cd2c680a551
rafal <rafal.kulaga93@gmail.com>
2018-06-27 20:40:40 +0200

    added missing library to visualLB.c and dealt with const issues

--- src/LBDefinitions.h
+++ src/LBDefinitions.h
@@ -26,17 +26,17 @@ static const int LATTICEVELOCITIES[19][3] = {
     { 1,  0,  1},
     { 0,  1,  1}
     
-}
+};
 
-double c0 = 12.0/36.0;
-double c1 =  2.0/36.0;
-double c2 =  1.0/36.0;
+#define c0  12.0/36.0 // TODO: Tudor will complain...
+#define c1  2.0/36.0
+#define c2  1.0/36.0
 
-static const double LATTICEWEIGHTS[19] = {c2, c2, c1, c2, c2, 
+static const double LATTICEWEIGHTS[19] = {c2, c2, c1, c2, c2,
                                           c2, c1, c2, c1, c0, 
                                           c1, c2, c1, c2, c2, 
                                           c2, c1, c2, c2};
     
-static const double C_S = 1.0/sqrt(3.0);
+static const double C_S = 1.0/1.73205; // 1.73205 is estimated sqrt(3)
 #endif
     
--- src/visualLB.c
+++ src/visualLB.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include "LBDefinitions.h"
 #include <math.h>
+#include "computeCellValues.h"
 
 void writeVtkOutput(const double * const collideField, const int * const flagField, 
                     const char* filename, int t, int xlength){
